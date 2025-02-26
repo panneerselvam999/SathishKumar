b32eea7007c385cd457f4d8ed9fd48fe4ba5be82
panneerselvam999 <npspanneerselvam999@gmail.com>
2025-02-26 14:02:45 +0530

    sss

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+// document.getElementById('nav-toggle').addEventListener('click', function () {
+//   const navList = document.querySelector('.nav__list');
+//   const nav__content = document.querySelector('.nav__content');
+
+//   navList.classList.toggle('active');
+//   nav__content.classList.toggle('active');
+// });
+
+document.getElementById('nav-toggle').addEventListener('click', function () {
+  const navList = document.querySelector('.nav__list');
+  navList.classList.toggle('active');
+});
